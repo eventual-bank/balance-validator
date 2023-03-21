586606523a91571cd6c0dcf113ecb03bc383cc8e
Eduardo Patrocinio <epatro@amazon.com>
2023-03-21 13:57:18 -0400

    Calculate sum

---
+++ .aws-sam/build/BalanceValidatorFunction/app.mjs
@@ -0,0 +1,40 @@
+import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
+import {
+  DynamoDBDocumentClient,
+  ScanCommand,
+  PutCommand,
+  GetCommand,
+  DeleteCommand,
+} from "@aws-sdk/lib-dynamodb";
+
+const client = new DynamoDBClient({});
+
+const dynamo = DynamoDBDocumentClient.from(client);
+
+const tableName = "account";
+
+export const handler = async (event, context) => {
+  let body;
+  let statusCode = 200;
+  const headers = {
+    "Content-Type": "application/json",
+  };
+
+  try {
+    body = await dynamo.send(
+      new ScanCommand({ TableName: tableName, AttributesToGet: ["balance"] })
+    );
+    body = body.Items;
+    body = body.reduce((a,b) => a+b.balance, 0)
+  } catch (err) {
+    statusCode = 400;
+    body = err.message;
+  } finally {
+    body = JSON.stringify(body);
+  }
+  return {
+    statusCode,
+    body,
+    headers,
+  };
+};
